faf0fa51638097febe7f2a5ce89e4fa0f4587e71
Domenick Casper <domenicktcasper@gmail.com>
2021-06-18 00:47:50 -0600

    Testing Streamlit a bit

---
+++ testing/domenick/streamlit.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+
+
+st.title('Testing Streamlit')
+
+st.radio('Radio', [1,2,3])
+st.selectbox('Select', [1,2,3])
+st.multiselect('Multiselect', [1,2,3])
+st.slider('Slide me', min_value=0, max_value=10)
+st.select_slider('Slide to select', options=[1,'2'])
+st.text_input('Enter some text')
+st.number_input('Enter a number')
+st.text_area('Area for textual entry')
+st.date_input('Date input')
+st.time_input('Time entry')
+st.file_uploader('File uploader')
+st.color_picker('Pick a color')
+
+st.balloons()
+
+#st.help() #DataFrame goes in here
