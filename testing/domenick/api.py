e4613bc3b5fa32ed685d53a252551e84192daa11
Domenick Casper <domenicktcasper@gmail.com>
2021-07-10 14:59:29 -0600

    y do

--- testing/domenick/api.py
+++ testing/domenick/api.py
@@ -44,7 +44,7 @@ def get_json_file(file):
 
 # Allows the user to select the dates they want displayed
 def get_dates():
-    years = [2000, 2001, 2002, 2003, 2004, 2005,
+    years = ['Year', 2000, 2001, 2002, 2003, 2004, 2005,
      2006, 2007, 2008, 2009, 2010, 2011, 2012,
       2013, 2014, 2015, 2016, 2017, 2018, 2019, 
       2020, 2021]
@@ -53,7 +53,7 @@ def get_dates():
     SD = st.selectbox('Start Year', options=years)
 
     #print(endYears)
-    if (SD):
+    if (SD == 'Year'):
         endYears = [year for year in years if year >= SD]
         ED = st.selectbox('End Year', options=endYears)
     
